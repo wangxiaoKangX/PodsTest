cb3d53156ce1de4ea2b428145ca2b4f887b5bd92
gitWxk <wxk@email.com>
2018-04-16 22:00:52 +0800

    pods2
    pods2

---
+++ XKPodsPerson/PersonTools.m
@@ -0,0 +1,13 @@
+//
+//  PersonTools.m
+//  PodsTest
+//
+//  Created by 王晓康 on 2018/4/16.
+//  Copyright © 2018年 wangxiaokang. All rights reserved.
+//
+
+#import "PersonTools.h"
+
+@implementation PersonTools
+
+@end
---
+++ XKPodsPerson/StudentTools.h
@@ -0,0 +1,13 @@
+//
+//  StudentTools.h
+//  PodsTest
+//
+//  Created by 王晓康 on 2018/4/16.
+//  Copyright © 2018年 wangxiaokang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface StudentTools : NSObject
+
+@end
