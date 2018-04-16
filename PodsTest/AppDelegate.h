70a1e535d8c5a298c44decff27b01e2600aa8972
gitWxk <wxk@email.com>
2018-04-16 21:58:38 +0800

    PodsT-1
    PodsT-1

---
+++ PodsTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PodsTest
+//
+//  Created by 王晓康 on 2018/4/16.
+//  Copyright © 2018年 wangxiaokang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
